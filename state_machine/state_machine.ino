68e16d9dfbfde11fe5889ef3e02d39b5e90f4944
aec273 <aec273@cornell.edu>
2018-11-27 18:44:54 -0500

    the code we had 2 months ago

--- state_machine/state_machine.ino
+++ state_machine/state_machine.ino
@@ -8,9 +8,9 @@ int frontWall = A3;
 int rightWall = A2;
 
 //CALIBRATED GLOBAL VARIABLES
-int lineVoltage = 700;
-int LRwalls = 200;
-int Fwall = 200;
+int lineVoltage = 800;
+int LRwalls = 120;
+int Fwall = 100;
 
 //NON-CALIBRATED GLOBAL VARIABLES
 Servo rightservo;
@@ -39,39 +39,6 @@ enum actions {
 states state = FOLLOW_LINE;
 actions action = DETECT_WALLS;
 
-enum maze_direction {
-  north,
-  south,
-  east,
-  west
-};
-
-int rows = 4; //length along y direction
-int cols = 5; //length along x direction
-char northWall = 0b00001000;
-char eastWall = 0b00000100;
-char southWall = 0b00000010;
-char westWall = 0b00000001;
-
-char front = 0b00001000;
-char right = 0b00000100;
-char left = 0b00000001;
-
-short maze[5][4] = 
-  { {0, 0, 0, 0},
-    {0, 0, 0, 0},
-    {0, 0, 0, 0},
-    {0, 0, 0, 0},
-    {0, 0, 0, 0},
-  };
-  
-int moves[60];
-int current[2]= {0, 0}; //first is x, second y
-int count = 0;
-bool backtracking = false;
-maze_direction c_direction = north;
-char relativeState = 0; // 1 means can go
-char absoluteState = 0; // 1 means can go
 
 /*Sets up servos*/
 void servoSetup()
@@ -86,7 +53,6 @@ void setup() {
   Serial.begin(9600);
   servoSetup();
   state = FOLLOW_LINE;
-  maze[0][0] = true;
 }
 
 int readL[3] = {lineVoltage + 100, lineVoltage + 100, lineVoltage + 100};
@@ -183,8 +149,6 @@ void turnLeft() {
 }
 
 void turnRight() {
-  Serial.println("Got Here");
-  delay(50);
   leftAverage = analogRead(sensorL);
   rightAverage = analogRead(sensorR);
   
@@ -213,7 +177,6 @@ void turnRight() {
   }
   leftservo.write(90);
   rightservo.write(90);
-  Serial.println("Done turning right");
 }
 
 bool detectRightWall() {
@@ -259,193 +222,23 @@ bool detectLeftWall() {
   }
 }
 
-char fourBitWrap(char x, int n)
-{
-  for (int a = 0; a < n; a++)
-  {
-    char temp = x & 0b00000001;
-    temp = temp << 3;
-    x = x >> 1;
-    x = x & 0b00001111;
-    x = x | temp;
-  }
-  return x;
-}
-
-char fourBitWrapLeft(char x, int n)
-{
-  for (int a = 0; a < n; a++)
-  {
-    char temp = x & 0b00001000;
-    temp = temp >> 3;
-    x = x << 1;
-    x = x & 0b00001111;
-    x = x | temp;
-  }
-  return x;
-}
-
-//i changed the !rWall to be just rWall because you want to or it when there is a wall
-void maze_info() {
-  if (rWall) {
-    relativeState = relativeState | right;
-  }
-  if (lWall) {
-    relativeState = relativeState | left;
-  }
-  if (fWall) {
-    relativeState = relativeState | front;
-  }
-
-  absoluteState = relativeState;
-
-  if (c_direction == east){
-    absoluteState = fourBitWrap(absoluteState, 1);
-  }
-  if (c_direction == south){
-    absoluteState = fourBitWrap(absoluteState, 2);
-  }
-  if (c_direction == west){
-    absoluteState = fourBitWrap(absoluteState, 3);
-  }
-
-  switch(c_direction) {
-    case north :
-      current[1] = current[1] + 1;
-      break;
-    case east :
-      current[0] = current[0] + 1;
-      break;
-    case south :
-      current[1] = current[1] - 1;
-      break;
-    case west :
-      current[0] = current[0] - 1;
-      break;
-  }
-
-  if (maze[current[0]][current[1]+1])
-  {
-    absoluteState = absoluteState & 0b11110111;
-  }
-  if (maze[current[0]+1][current[1]])
-  {
-    absoluteState = absoluteState & 0b11111011;
-  }
-  if (maze[current[0]][current[1]-1])
-  {
-    absoluteState = absoluteState & 0b11111101;
-  }
-  if (maze[current[0]-1][current[1]])
-  {
-    absoluteState = absoluteState & 0b11111110;
-  }
-  
-  maze[current[0]][current[1]] = true;
-
-  relativeState = absoluteState;
-
-  if (c_direction == east){
-    relativeState = fourBitWrapLeft(relativeState, 1);
-  }
-  if (c_direction == south){
-    relativeState = fourBitWrapLeft(relativeState, 2);
-  }
-  if (c_direction == west){
-    relativeState = fourBitWrapLeft(relativeState, 3);
-  }
-}
-
-void backtrack(){
-  if (backtracking == false)
-  {
-    follow();
-  }
-  backtracking = true;
-  if (moves[count] == 1)
-  {
-    moves[count] = 0;
-    count = count - 1;
+void turns() {
+  if ( rWall && lWall && fWall ) {
+    Serial.println("walls on all sides");
+    turnLeft();
     turnLeft();
   }
-  else if (moves[count] == 2)
-  {
-    moves[count] = 0;
-    count = count - 1;
+  else if ( rWall && fWall && !lWall ) {
+    Serial.println("no left wall");
+    turnLeft();
   }
-  else if (moves[count] == 3){
-    moves[count] = 0;
-    count = count - 1;
+  else if ( !rWall ) {
+    Serial.println("no right wall");
     turnRight();
   }
-}
-
-void turns() {
-//  if ((relativeState & 0b00000100) > 1) {
-//    turnRight();
-//    moves[count] = 1; //right
-//    backtracking = false;
-//  }
-//  else if ((relativeState & 0b00001000) > 1) {
-//    forward();
-//    count++;
-//    moves[count] = 2; //straight
-//    backtracking = false;
-//  }
-//  else if ((relativeState & 0b00000001) > 1) {
-//    turnLeft();
-//    moves[count] = 3; //left
-//    backtracking = false;
-//  }
-//  else {
-//    if (backtracking) {
-//      backtrack();
-//    }
-//    else {
-//      //uturn
-//      turnRight();
-//      turnRight();
-//      backtrack();
-//    }
-//  }
-////  if ( rWall && lWall && fWall ) {
-////    Serial.println("walls on all sides");
-////    turnLeft();
-////    turnLeft();
-////  }
-////  else if ( rWall && fWall && !lWall ) {
-////    Serial.println("no left wall");
-////    turnLeft();
-////  }
-////  else if ( !rWall ) {
-////    Serial.println("no right wall");
-////    turnRight();
-////  }
-////  else {
-////    
-////    forward();
-////  }
-  // U-turn
-    if (fWall && lWall && rWall) {
-      Serial.println("U-Turn");
-      turnRight();
-      turnRight();
-    }
-    // Left Turn
-    else if (fWall && !lWall && rWall) {
-      Serial.println("Left Turn");
-      turnLeft();
-    }
-    // Right Turn
-    else if (!rWall) {
-      Serial.println("Right Turn");
-      turnRight();
-    }
-    // Go forward
-    else {
-      leftservo.write(135);
-      rightservo.write(45);
-    }
+  else {
+    forward();
+  }
 }
 
 void loop() {
@@ -476,33 +269,16 @@ void loop() {
     case INTERSECTION:
       switch (action) {
         case DETECT_WALLS :
-          Serial.println("Detect Walls");
+          Serial.println("detecting intersection");
           leftservo.write(90);
           rightservo.write(90);
           delay(1000); 
           lWall = detectLeftWall();
           rWall = detectRightWall();
-          if (rWall) {
-            Serial.println("right wall");
-          }
-          else if ( lWall ) {
-            Serial.println("left wall");
-          }
           fWall = detectFrontWall();
-          if ( fWall ) {
-            Serial.println("front Wall");
-          }
-          action = CHECK;
-          break;
-        case CHECK :
-          //maze_info();
-          action = MOVE;
-          break;
         case MOVE : 
           stepPast();
           turns();
-          action = DETECT_WALLS;
-          break;
         default :
           state = FOLLOW_LINE;
           break;
